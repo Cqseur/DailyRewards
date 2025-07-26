bdaf451bb072d3189a81cf88bacd114351708832
Cqseur <54004740+Cqseur@users.noreply.github.com>
2025-07-26 12:23:45 +0200

    ?

--- src/client/kotlin/cqseur/dailyrewards/DailyRewardsModMenu.kt
+++ src/client/kotlin/cqseur/dailyrewards/DailyRewardsModMenu.kt
@@ -4,13 +4,8 @@ import com.terraformersmc.modmenu.api.ModMenuApi
 import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import net.minecraft.client.gui.screen.Screen
 import cqseur.dailyrewards.ui.DailyRewardsConfigScreen
-<<<<<<< Updated upstream
 
-object DailyRewardsModMenu : ModMenuApi {
-=======
- 
 class DailyRewardsModMenu : ModMenuApi {
->>>>>>> Stashed changes
     override fun getModConfigScreenFactory(): ConfigScreenFactory<Screen> {
         return ConfigScreenFactory { parent: Screen? ->
             DailyRewardsConfigScreen.create(parent)
